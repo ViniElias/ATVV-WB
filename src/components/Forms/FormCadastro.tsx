6b562952ee354fd643beac7936ae1296c1054cf3
ViniElias <viniciuseliascr@gmail.com>
2025-04-30 08:40:57 -0300

    Add formatação do formulário

--- src/components/Forms/FormCadastro.tsx
+++ src/components/Forms/FormCadastro.tsx
@@ -7,6 +7,7 @@ type State = {
     genero: string;
     numeroCpf: string;
     dataCpf: string;
+    mensagem: string;
 };
 
 export default class FormCadastro extends Component<{}, State> {
@@ -15,45 +16,89 @@ export default class FormCadastro extends Component<{}, State> {
         nomeSocial: '',
         genero: '',
         numeroCpf: '',
-        dataCpf: ''
+        dataCpf: '',
+        mensagem: ''
     };
 
     handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = event.target;
-        this.setState({[name]: value} as Pick<State, keyof State>);
+        const { name, value } = event.target;
+        this.setState({ [name]: value } as Pick<State, keyof State>);
     };
 
     handleSubmit = (event: FormEvent) => {
         event.preventDefault();
 
+        this.setState({
+            nome: '',
+            nomeSocial: '',
+            genero: '',
+            numeroCpf: '',
+            dataCpf: '',
+            mensagem: 'Cliente cadastrado com sucesso!'
+        });
     };
     render() {
-        const {nome, nomeSocial, genero, numeroCpf, dataCpf} = this.state;
+        const { nome, nomeSocial, genero, numeroCpf, dataCpf } = this.state;
 
         return (
             <div className="form">
                 <h3>Cadastrar</h3>
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="nome">Nome</label>
-                    <input type="text" id="nome" name="nome" value={nome} onChange={this.handleChange} required/>
+                    <input
+                        type="text" id="nome" name="nome"
+                        value={nome} onChange={this.handleChange}
+                        required pattern="[A-Za-zÀ-ÿ\s]+"
+                        title="Apenas letras e espaços são permitidos"
+                    />
 
                     <label htmlFor="nomeSocial">Nome social</label>
-                    <input type="text" id="nomeSocial" name="nomeSocial" value={nomeSocial} onChange={this.handleChange} required/>
+                    <input
+                        type="text" id="nomeSocial" name="nomeSocial"
+                        value={nomeSocial} onChange={this.handleChange}
+                        required pattern="[A-Za-zÀ-ÿ\s]+"
+                        title="Apenas letras e espaços são permitidos"
+                    />
 
-                    <p>Gênero</p>
-                    <label htmlFor="nome">Masculino</label>
-                    <input type="radio" id="masculino" name="genero" value="Masculino" checked={genero === 'Masculino'} onChange={this.handleChange} required/>
-                    <label htmlFor="nome">Feminino</label>
-                    <input type="radio" id="feminino" name="genero" value="Feminino"  checked={genero === 'Feminino'} onChange={this.handleChange} required/>
+                    <fieldset id="fieldGenero">
+                        <legend>Gênero</legend>
+                        <div>
+                            <input type="radio" id="masculino" name="genero" value="Masculino" checked={genero === 'Masculino'} onChange={this.handleChange} required />
+                            <label htmlFor="nome">Masculino</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="feminino" name="genero" value="Feminino" checked={genero === 'Feminino'} onChange={this.handleChange} required />
+                            <label htmlFor="nome">Feminino</label>
+                        </div>
+                    </fieldset>
 
-                    <h4>CPF</h4>
-                    <label htmlFor="cpf">Número</label>
-                    <input type="text" id="numeroCpf" name="numeroCpf" value={numeroCpf} onChange={this.handleChange} required/>
-                    <label htmlFor="nome">Data de emissão</label>
-                    <input type="date" id="dataCpf" name="dataCpf" value={dataCpf} onChange={this.handleChange} required/>
+                    <fieldset>
+                        <legend>CPF</legend>
+                        <div className="cpfGroup">
+                            <div>
+                                <label htmlFor="cpf">Número </label>
+                                <input
+                                    type="text" id="numeroCpf" name="numeroCpf"
+                                    value={numeroCpf} onChange={this.handleChange}
+                                    required pattern="\d{11}" maxLength={11}
+                                    title="Apenas números são aceitos."
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="nome">Data de emissão </label>
+                                <input
+                                    type="date" id="dataCpf" name="dataCpf"
+                                    value={dataCpf} onChange={this.handleChange} required
+                                    min="1925-01-01" max={new Date().toISOString().split('T')[0]} />
+                            </div>
+                        </div>
+                    </fieldset>
 
                     <button type="submit">Cadastrar</button>
                 </form>
+                {this.state.mensagem && (
+                    <p className="mensagem">{this.state.mensagem}</p>
+                )}
             </div>
         );
     };
