b8ddb807af2438da83453ad124aab8d00618daba
ViniElias <viniciuseliascr@gmail.com>
2025-06-02 10:40:47 -0300

    add pagina de relatorios

--- src/pages/Relatorios.tsx
+++ src/pages/Relatorios.tsx
@@ -36,23 +36,35 @@ export default class Relatorios extends Component<{}, State> {
         <h2 className="page-title">Relatórios</h2>
         <div className="cards">
           <Card
-            title="Cadastrar"
-            text="Adicione um novo produto ao sistema."
+            title="Maiores consumidores em quantidade"
+            text="Liste os 10 clientes que mais compraram (em quantidade, não valor)."
             click={() => this.selectCard('Cadastrar Produto')}
           />
           <Card
-            title="Excluir"
-            text="Remova um produto já existente."
+            title="Listar todos os clientes"
+            text="Liste todos os clientes cadastrados, ordenados por gênero."
             click={() => this.selectCard('Excluir Produto')}
           />
           <Card
-            title="Atualizar"
-            text="Atualize os dados de um produto."
+            title="Produtos e serviços mais consumidos"
+            text="Liste os produtos e serviços mais consumidos no geral."
             click={() => this.selectCard('Atualizar Produto')}
           />
+        </div>
+        <div className="cards">
           <Card
-            title="Listar"
-            text="Veja a lista de todos os produtos cadastrados."
+            title="Produtos e serviços por gênero"
+            text="Liste os produtos e serviços mais consumidos por gênero."
+            click={() => this.selectCard('Listar Produtos')}
+          />
+          <Card
+            title="Menores consumidores em quantidade"
+            text="Liste os 10 clientes que menos compraram (em quantidade, não valor)."
+            click={() => this.selectCard('Listar Produtos')}
+          />
+          <Card
+            title="Maiores consumidores em valor"
+            text="Liste os 5 clientes que mais gastaram."
             click={() => this.selectCard('Listar Produtos')}
           />
         </div>
